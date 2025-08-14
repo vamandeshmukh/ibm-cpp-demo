98bf6c59dfaea570e2b0324adab12a8782af8ef2
Vaman Deshmukh <vamandeshmukh@gmail.com>
2025-08-14 12:46:51 +0530

    initial commit

--- cp/demo.cpp
+++ cp/demo.cpp
@@ -1,94 +1,102 @@
-// version 6
 #include <iostream>
+int main()
+{
+    int num = 10;
+
+    return 0;
+}
+
+// // version 6
+// #include <iostream>
 
 // datatypes, variable declaration, opertors, separators, comments,
 // control structures, arrays
 // function declaration, function invocation
 
-int main()
-{
-    int num = 0; // unsigned
-    signed int num2 = 1;
-    unsigned int num3 = 2;
-
-    return 0;
-}
-
-// // // version 6
-// // #include <iostream>
-
-// // int main()
-// // {
-// //     int num = 10;
-// //     char ch = 'a';
-// //     std::cout << ch + num;
-// //     return 0;
-// // }
-
-// // version 5
-// #include <iostream>
-
 // int main()
 // {
-//     for (char b = 0; b < 255; ++b) {
-//         std::cout << (int) b << " ";
-//     }
-//     std::cout << std::endl;
+//     int num = 0; // unsigned
+//     signed int num2 = 1;
+//     unsigned int num3 = 2;
+
 //     return 0;
 // }
 
-// // // version 4
+// // // // version 6
+// // // #include <iostream>
+
+// // // int main()
+// // // {
+// // //     int num = 10;
+// // //     char ch = 'a';
+// // //     std::cout << ch + num;
+// // //     return 0;
+// // // }
+
+// // // version 5
 // // #include <iostream>
 
 // // int main()
 // // {
-// //     int id;
-// //     std::string name;
-// //     double salary;
-
-// //     std::cout << "Enter your id: ";
-// //     std::cin >> id;
-// //     std::cout << "Enter your name: "; // Sonu Joshi
-// //     std::cin.ignore();
-// //     std::getline(std::cin, name);
-// //     std::cout << "Enter your salary: ";
-// //     std::cin >> salary;
-// //     std::cout << id << " " << name << " " << salary << std::endl;
+// //     for (char b = 0; b < 255; ++b) {
+// //         std::cout << (int) b << " ";
+// //     }
+// //     std::cout << std::endl;
 // //     return 0;
 // // }
 
-// // // version 3
-// // #include <iostream>
-
-// // int main() {
-// //     std::string name;
-// //     std::cout << "Enter your name: "; // Sonu Joshi
-// //     std::cin >> name; // this
-// //     // std::getline(std::cin, name); // and this
-// //     std::cout << "Hello, " << name << "!" << std::endl;
-// //     return 0;
-// // }
-
-// // // version 2
-// // #include <iostream>
-
-// // int main() {
-// //     std::string name;
-// //     std::cout << "Enter your name: ";
-// //     std::getline(std::cin, name);
-// //     std::cout << "Hello, " << name << "!" << std::endl;
-// //     return 0;
-// // }
-
-// // // // version 1
+// // // // version 4
 // // // #include <iostream>
-// // // using namespace std;
-// // // // import java.util.*;
 
-// // // int main() {
-// // //     string name;
-// // //     cout << "Enter your name: ";
-// // //     getline(cin, name);
-// // //     cout << "Hello, " << name << "!" << endl;
+// // // int main()
+// // // {
+// // //     int id;
+// // //     std::string name;
+// // //     double salary;
+
+// // //     std::cout << "Enter your id: ";
+// // //     std::cin >> id;
+// // //     std::cout << "Enter your name: "; // Sonu Joshi
+// // //     std::cin.ignore();
+// // //     std::getline(std::cin, name);
+// // //     std::cout << "Enter your salary: ";
+// // //     std::cin >> salary;
+// // //     std::cout << id << " " << name << " " << salary << std::endl;
 // // //     return 0;
 // // // }
+
+// // // // version 3
+// // // #include <iostream>
+
+// // // int main() {
+// // //     std::string name;
+// // //     std::cout << "Enter your name: "; // Sonu Joshi
+// // //     std::cin >> name; // this
+// // //     // std::getline(std::cin, name); // and this
+// // //     std::cout << "Hello, " << name << "!" << std::endl;
+// // //     return 0;
+// // // }
+
+// // // // version 2
+// // // #include <iostream>
+
+// // // int main() {
+// // //     std::string name;
+// // //     std::cout << "Enter your name: ";
+// // //     std::getline(std::cin, name);
+// // //     std::cout << "Hello, " << name << "!" << std::endl;
+// // //     return 0;
+// // // }
+
+// // // // // version 1
+// // // // #include <iostream>
+// // // // using namespace std;
+// // // // // import java.util.*;
+
+// // // // int main() {
+// // // //     string name;
+// // // //     cout << "Enter your name: ";
+// // // //     getline(cin, name);
+// // // //     cout << "Hello, " << name << "!" << endl;
+// // // //     return 0;
+// // // // }
