2ca8a4dfc42c9dd761e1a6cdd2b61ef451717c16
Vaman Deshmukh <vamandeshmukh@gmail.com>
2025-08-18 14:58:26 +0530

    pure virtual functions

--- cp/day2structs2.cpp
+++ cp/day2structs2.cpp
@@ -8,16 +8,14 @@ struct Employee
     string name;
     double salary;
 
-    virtual void printBonus()
-    {
-        cout << "Bonus received by employee!" << endl;
-    }
+    // pure virtual function in C++ == abstract method in Java
+    virtual void printBonus() = 0;
 };
 
 struct ContractEmployee : public Employee
 {
 
-    virtual void printBonus() override
+    void printBonus() override
     {
         cout << "No bonus for contractuals!" << endl;
     }
@@ -25,8 +23,8 @@ struct ContractEmployee : public Employee
 
 int main()
 {
-    Employee emp1;
-    emp1.printBonus();
+    // Employee emp1;
+    // emp1.printBonus();
     ContractEmployee emp2;
     emp2.printBonus();
     return 0;
@@ -42,23 +40,58 @@ int main()
 //     string name;
 //     double salary;
 
-//     void printData()
+// Virtual function to be overridden in derived classes
+//     virtual void printBonus()
 //     {
-//         cout << id << " " << name << " " << salary << endl;
+//         cout << "Bonus received by employee!" << endl;
 //     }
+// };
 
-//     virtual void printBonus();
+// struct ContractEmployee : public Employee
+// {
 
-//     // virtual void printBonus()
-//     // {
-//     //     cout << salary * 2 << endl;
-//     // }
+//     void printBonus() override
+//     {
+//         cout << "No bonus for contractuals!" << endl;
+//     }
 // };
 
 // int main()
 // {
-//     // Employee emp1 = {101, "Tonu Pandey", 90000.00};
-//     // emp1.printData();
-//     // // emp1.printBonus();
+//     Employee emp1;
+//     emp1.printBonus();
+//     ContractEmployee emp2;
+//     emp2.printBonus();
 //     return 0;
 // }
+
+// // #include <iostream>
+// // #include <string>
+// // using namespace std;
+
+// // struct Employee
+// // {
+// //     int id;
+// //     string name;
+// //     double salary;
+
+// //     void printData()
+// //     {
+// //         cout << id << " " << name << " " << salary << endl;
+// //     }
+
+// //     virtual void printBonus();
+
+// //     // virtual void printBonus()
+// //     // {
+// //     //     cout << salary * 2 << endl;
+// //     // }
+// // };
+
+// // int main()
+// // {
+// //     // Employee emp1 = {101, "Tonu Pandey", 90000.00};
+// //     // emp1.printData();
+// //     // // emp1.printBonus();
+// //     return 0;
+// // }
