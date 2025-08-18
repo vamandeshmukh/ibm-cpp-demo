aeca1cc3661ec0e294a67a8c528cc86c2b418155
Vaman Deshmukh <vamandeshmukh@gmail.com>
2025-08-18 10:24:08 +0530

    cpp day 2

--- cp/day2structs.cpp
+++ cp/day2structs.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
 using namespace std;
 
+struct Employee
+{
+    int id;
+    string name;
+    double salary;
+};
+
 int main()
 {
-
-    struct Employee
-    {
-        int id;
-        string name;
-        double salary;
-    };
+    Employee emp;
+    emp.id = 101;
+    emp.name = "Sonu";
+    emp.salary = 50000.50;
+    cout << emp.id << "" << emp.name << " " << emp.salary << endl;
 
     return 0;
 }
