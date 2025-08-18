54dd5dec589cfb1454345cafd915fe74257b92ce
Vaman Deshmukh <vamandeshmukh@gmail.com>
2025-08-18 10:37:51 +0530

    cpp day 2 structs

--- cp/day2structs.cpp
+++ cp/day2structs.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 using namespace std;
 
+struct Address
+{
+    string street;
+    string city;
+    int pin;
+};
+
 struct Employee
 {
     int id;
     string name;
     double salary;
+    // array ? // phones 
+    Address address;
+    // functions ? to print data 
 };
 
 int main()
@@ -14,7 +24,13 @@ int main()
     emp.id = 101;
     emp.name = "Sonu";
     emp.salary = 50000.50;
+    emp.address.street = "2, Whites Road";
+    emp.address.city = "Chennai";
+    emp.address.pin = 600014;
     cout << emp.id << " " << emp.name << " " << emp.salary << endl;
+    cout << emp.address.street << " " << emp.address.city << " " << emp.address.pin << endl;
+
+    Employee emp2 = {102, "Monu", 5500075, {"Thanisandra", "Bengaluru", 500063}};
 
     return 0;
 }
