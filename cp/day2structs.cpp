cfa615f9932beaa20b3992c960f3ae4577886870
Vaman Deshmukh <vamandeshmukh@gmail.com>
2025-08-18 11:44:20 +0530

    vectors

--- cp/day2structs.cpp
+++ cp/day2structs.cpp
@@ -1,4 +1,6 @@
+// using vector
 #include <iostream>
+#include <vector>
 using namespace std;
 
 struct Address
@@ -13,24 +15,173 @@ struct Employee
     int id;
     string name;
     double salary;
-    // array ? // phones 
+    vector<string> phones;
     Address address;
-    // functions ? to print data 
+    void printData()
+    {
+        cout << id << " " << name << " " << salary << endl;
+        // use phones.size();
+
+        cout << phones[0] << " " << phones[1] << endl;
+        cout << address.street << " " << address.city << " " << address.pin << endl;
+    }
+    double calcBonus(int bonusPct)
+    {
+        return salary + (salary * bonusPct / 100);
+    }
 };
 
 int main()
 {
-    Employee emp;
-    emp.id = 101;
-    emp.name = "Sonu";
-    emp.salary = 50000.50;
-    emp.address.street = "2, Whites Road";
-    emp.address.city = "Chennai";
-    emp.address.pin = 600014;
-    cout << emp.id << " " << emp.name << " " << emp.salary << endl;
-    cout << emp.address.street << " " << emp.address.city << " " << emp.address.pin << endl;
 
-    Employee emp2 = {102, "Monu", 5500075, {"Thanisandra", "Bengaluru", 500063}};
+    Employee emp1 = {101, "Tonu Pandey", 90000.00, {"9822012345", "9876543210"}, {"Thanisandra", "Bengaluru", 500063}};
+    emp1.printData();
+    double bonus = emp1.calcBonus(10);
+    cout << bonus << endl;
+
+    Employee emp2 = {102, "Monu Reddy", 80000.00, {"6789012345"}, {"Thanisandra", "Bengaluru", 500063}};
+    emp2.phones.push_back("6789012345");
+    emp2.phones.size();
+
+    emp2.printData();
+    bonus = emp2.calcBonus(5);
+    cout << bonus << endl;
 
     return 0;
 }
+
+// #include <iostream>
+// using namespace std;
+
+// struct Address
+// {
+//     string street;
+//     string city;
+//     int pin;
+// };
+
+// struct Employee
+// {
+//     int id;
+//     string name;
+//     double salary;
+//     long long phones[2];
+//     Address address;
+//     void printData()
+//     {
+//         cout << id << " " << name << " " << salary << endl;
+//         cout << phones[0] << " " << phones[1] << endl;
+//         cout << address.street << " " << address.city << " " << address.pin << endl;
+//     }
+//     double calcBonus(int bonusPct)
+//     {
+//         return salary + (salary * bonusPct / 100);
+//     }
+// };
+
+// int main()
+// {
+
+//     Employee emp1 = {101, "Tonu Pandey", 90000.00, {9822012345L, 9876543210L}, {"Thanisandra", "Bengaluru", 500063}};
+//     emp1.printData();
+//     double bonus = emp1.calcBonus(10);
+//     cout << bonus << endl;
+
+//     Employee emp2 = {102, "Monu Reddy", 80000.00, {6789012345L}, {"Thanisandra", "Bengaluru", 500063}};
+//     emp2.printData();
+//     bonus = emp2.calcBonus(5);
+//     cout << bonus << endl;
+
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// struct Address
+// {
+//     string street;
+//     string city;
+//     int pin;
+// };
+
+// struct Employee
+// {
+//     int id;
+//     string name;
+//     double salary;
+//     string phones[2];
+//     Address address;
+//     void printData()
+//     {
+//         cout << id << " " << name << " " << salary << endl;
+//         cout << phones[0] << " " << phones[1] << endl;
+//         cout << address.street << " " << address.city << " " << address.pin << endl;
+//     }
+//     double calcBonus(int bonusPct)
+//     {
+//         return salary + (salary * bonusPct / 100);
+//     }
+// };
+
+// int main()
+// {
+
+//     Employee emp1 = {101, "Tonu Pandey", 90000.00, {"9822012345", "9876543210"}, {"Thanisandra", "Bengaluru", 500063}};
+//     emp1.printData();
+//     double bonus = emp1.calcBonus(10);
+//     cout << bonus << endl;
+
+//     Employee emp2 = {102, "Monu Reddy", 80000.00, {"6789012345"}, {"Thanisandra", "Bengaluru", 500063}};
+//     emp2.printData();
+//     bonus = emp2.calcBonus(5);
+//     cout << bonus << endl;
+
+//     return 0;
+// }
+
+// // #include <iostream>
+// // using namespace std;
+
+// // struct Address
+// // {
+// //     string street;
+// //     string city;
+// //     int pin;
+// // };
+
+// // struct Employee
+// // {
+// //     int id;
+// //     string name;
+// //     double salary;
+// //     string phones[2];
+// //     Address address;
+// //     void printData()
+// //     {
+// //         cout << id << " " << name << " " << salary << endl;
+// //         cout << phones[0] << " " << phones[1] << endl;
+// //         cout << address.street << " " << address.city << " " << address.pin << endl;
+// //     }
+// // };
+
+// // int main()
+// // {
+
+// //     Employee emp3 = {102, "Tonu", 100, {"9822012345", "9876543210"}, {"Thanisandra", "Bengaluru", 500063}};
+// //     emp3.printData();
+
+// //     Employee emp;
+// //     emp.id = 101;
+// //     emp.name = "Sonu";
+// //     emp.salary = 50000.50;
+// //     emp.address.street = "2, Whites Road";
+// //     emp.address.city = "Chennai";
+// //     emp.address.pin = 600014;
+// //     cout << emp.id << " " << emp.name << " " << emp.salary << endl;
+// //     cout << emp.address.street << " " << emp.address.city << " " << emp.address.pin << endl;
+
+// //     // Employee emp2 = {102, "Monu", 5500075, {"Thanisandra", "Bengaluru", 500063}};
+
+// //     return 0;
+// // }
