18344598f5e37d777f61555b5b4ab4e59e20dc10
Vaman Deshmukh <vamandeshmukh@gmail.com>
2025-08-19 11:30:11 +0530

    classes and its members

--- cp/day3class.cpp
+++ cp/day3class.cpp
@@ -1,6 +1,3 @@
-#include <iostream>
-using namespace std;
-
 // class -
 // members - fields, methods, constructors, inner classes
 // access - private
@@ -8,35 +5,25 @@ using namespace std;
 // ======================
 // class - blueprint for creating objects
 
+#include <iostream>
+using namespace std;
+
 class Employee
 {
-
-private:
-    static string officePhone;
-
-private:
-    double salary;
 
 public:
     int id;
     string name;
-
-    // member initializer list
-    Employee(int i, string n, double s) : id(i), name(n), salary(s) {}
+    double salary;
 
     Employee() {}
 
-    //  assignment inside the body
     Employee(int i, string n, double s)
     {
         id = i;
         name = n;
         salary = s;
     }
-
-    class SubEmployee
-    {
-    };
 
     void display()
     {
@@ -51,8 +38,71 @@ int main()
     Employee *emp3 = new Employee();
     Employee emp4;
     emp1.display();
-    // emp2.display();
-    // emp3.display();
+    emp2->display();
+    emp3->display();
+    emp4.display();
+
+    delete emp2;
+    delete emp3;
 
     return 0;
 }
+
+// #include <iostream>
+// using namespace std;
+
+// // class -
+// // members - fields, methods, constructors, inner classes
+// // access - private
+// // object - stack / heap
+// // ======================
+// // class - blueprint for creating objects
+
+// class Employee
+// {
+
+// private:
+//     static string officePhone;
+
+// private:
+//     double salary;
+
+// public:
+//     int id;
+//     string name;
+
+//     // member initializer list
+//     Employee(int i, string n, double s) : id(i), name(n), salary(s) {}
+
+//     Employee() {}
+
+//     //  assignment inside the body
+//     Employee(int i, string n, double s)
+//     {
+//         id = i;
+//         name = n;
+//         salary = s;
+//     }
+
+//     class SubEmployee
+//     {
+//     };
+
+//     void display()
+//     {
+//         cout << "ID: " << id << ", Name: " << name << ", Salary: " << salary << endl;
+//     }
+// };
+
+// int main()
+// {
+//     Employee emp1(101, "Sonu", 50000.0);
+//     Employee *emp2 = new Employee(102, "Monu", 60000.0);
+//     Employee *emp3 = new Employee();
+//     Employee emp4;
+//     emp1.display();
+//     // emp2.display();
+//     // emp3.display();
+
+//     return 0;
+// }
