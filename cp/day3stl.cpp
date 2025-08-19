117ad978d2864a3142a2bd1c8b0464a06d59b07d
Vaman Deshmukh <vamandeshmukh@gmail.com>
2025-08-19 15:24:19 +0530

    stl

--- cp/day3stl.cpp
+++ cp/day3stl.cpp
@@ -1,11 +1,29 @@
+// set, queue, list, map, unordered_map, stack, priority_queue
+
+// map - user: rides_list  
+// queue - riders_queue 
+// queue - 
+
 #include <iostream>
-#include <vector>
-// set queue, list, map, unordered_map, stack, priority_queue
+#include <vector> // 
+#include <map> // 
+#include <set> // 
+#include <list> // 
+#include <unordered_map> // 
 
 using namespace std;
 
 int main()
 {
+
+    map<string, string> codes;
+    codes["KA"] = "Karnataka";
+    codes["MH"] = "Maharashtra";
+    codes["TG"] = "Telangana";
+    codes["AP"] = "Andhra Pradesh";
+
+    string state = codes.at("KA");
+    cout << state << endl;
 
     vector<int> nums = {98, 96, 99, 95, 97};
     for (int num : nums)
@@ -16,6 +34,8 @@ int main()
     nums.push_back(99);
     nums.size();
     nums.pop_back();
-
+    nums.insert(nums.begin() + 2, 100);
+    nums.at(2) = 101;
+    nums.erase(nums.begin() + 2);
     return 0;
 }
