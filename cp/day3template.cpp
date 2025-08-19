4894b2adb07b5cf6de0916caf875a3dff1008235
Vaman Deshmukh <vamandeshmukh@gmail.com>
2025-08-19 12:35:00 +0530

    exception handling

--- cp/day3template.cpp
+++ cp/day3template.cpp
@@ -11,10 +11,16 @@ int main()
 {
     int num = add(10, 20);
     cout << num << endl;
-    string num2 = add("abc", "def");
+    double num2 = add(3.50, 4.75);
     cout << num2 << endl;
+    // string num3 = add("aaa", "bbb");
+    // cout << num3 << endl;
     return 0;
 }
+
+
+
+
 
 // #include <iostream>
 // using namespace std;
