818f4f57bce32daaf43ca435fe338182561b1e67
Vaman Deshmukh <vamandeshmukh@gmail.com>
2025-08-19 09:41:05 +0530

    operator overloading

--- cp/day2.cpp
+++ cp/day2.cpp
@@ -1,68 +1,136 @@
 #include <iostream>
-#include <memory>
 using namespace std;
 
-// Which one to use?
+class Complex
+{
+private:
+    int real;
+    int imag;
+
+public:
+    // Constructor
+    Complex(int r = 0, int i = 0) : real(r), imag(i) {}
+
+    // Operator overloading for +
+    Complex operator+(const Complex &other)
+    {
+        return Complex(real + other.real, imag + other.imag);
+    }
+
+    // Function to display complex number
+    void display()
+    {
+        cout << real << " + " << imag << "i" << endl;
+    }
+};
 
 int main()
 {
+    Complex c1(3, 4);
+    Complex c2(1, 2);
 
-    int* ptr = new int(10); // old fashioned pointer
-    cout << *ptr << endl;
-    unique_ptr<int> smrtPtr(new int(20)); // smart pointer 1
-    cout << *smrtPtr << endl;
-    shared_ptr<int> sharedPtr(new int(30)); // smart pointer 2
-    cout << *sharedPtr << endl;
-    delete ptr; // delete
+    Complex c3 = c1 + c2; // Uses overloaded + operator
+
+    c3.display(); // Output: 4 + 6i
     return 0;
 }
 
 // #include <iostream>
-// #include <memory>
 // using namespace std;
+
+// class Complex
+// {
+// private:
+//     float real, imag;
+
+// public:
+//     Complex(float r = 0, float i = 0) : real(r), imag(i) {}
+
+//     // Overload the '+' operator
+//     Complex operator+(const Complex &other)
+//     {
+//         return Complex(real + other.real, imag + other.imag);
+//     }
+
+//     void display() const
+//     {
+//         cout << real << " + " << imag << "i" << endl;
+//     }
+// };
 
 // int main()
 // {
-//     // memory management
-//     // smart pointers
-//     unique_ptr<int> smrtPtr(new int(10)); // unique pointer
-//     cout << *smrtPtr << endl; // Accessing value through unique pointer
+//     Complex c1(3.5, 2.5);
 
-//     shared_ptr<int> sharedPtr(new int(30)); // shared pointer
-//     cout << *sharedPtr << endl; // Accessing value through shared pointer
-
+//     Complex c2(1.5, 4.5);
 //     return 0;
 // }
 
-// #include <iostream>
-// using namespace std;
+// // #include <iostream>
+// // #include <memory>
+// // using namespace std;
 
-// int main()
-// {
-//     // memory management
-//     int* ptr = new int;
-//     int arr[5];
+// // // Which one to use?
 
-//     int n = 5; // dynamic
-//     int* numArray = new int[n];
+// // int main()
+// // {
 
-//     int num = 10;
-//     cout << num << endl;
-//     int* numPtr = &num; // Pointer to num
-//     cout << *numPtr << endl;
-//     cout << numPtr << endl; // Address of num
-//     return 0;
-// }
+// //     int* ptr = new int(10); // old fashioned pointer
+// //     cout << *ptr << endl;
+// //     unique_ptr<int> smrtPtr(new int(20)); // smart pointer 1
+// //     cout << *smrtPtr << endl;
+// //     shared_ptr<int> sharedPtr(new int(30)); // smart pointer 2
+// //     cout << *sharedPtr << endl;
+// //     delete ptr; // delete
+// //     return 0;
+// // }
 
-// #include <iostream>
-// using namespace std;
+// // // #include <iostream>
+// // // #include <memory>
+// // // using namespace std;
 
-// int main()
-// {
-//     int num = 10;
-//     cout << num << endl;
-//     int* numPtr = &num; // Pointer to num
-//     cout << *numPtr << endl;
-//     cout << numPtr << endl; // Address of num
-//     return 0;
-// }
+// // // int main()
+// // // {
+// // //     // memory management
+// // //     // smart pointers
+// // //     unique_ptr<int> smrtPtr(new int(10)); // unique pointer
+// // //     cout << *smrtPtr << endl; // Accessing value through unique pointer
+
+// // //     shared_ptr<int> sharedPtr(new int(30)); // shared pointer
+// // //     cout << *sharedPtr << endl; // Accessing value through shared pointer
+
+// // //     return 0;
+// // // }
+
+// // // #include <iostream>
+// // // using namespace std;
+
+// // // int main()
+// // // {
+// // //     // memory management
+// // //     int* ptr = new int;
+// // //     int arr[5];
+
+// // //     int n = 5; // dynamic
+// // //     int* numArray = new int[n];
+
+// // //     int num = 10;
+// // //     cout << num << endl;
+// // //     int* numPtr = &num; // Pointer to num
+// // //     cout << *numPtr << endl;
+// // //     cout << numPtr << endl; // Address of num
+// // //     return 0;
+// // // }
+
+// // // #include <iostream>
+// // // using namespace std;
+
+// // // int main()
+// // // {
+// // //     int num = 10;
+// // //     cout << num << endl;
+// // //     int* numPtr = &num; // Pointer to num
+// // //     cout << *numPtr << endl;
+// // //     cout << numPtr << endl; // Address of num
+// // //     return 0;
+// // // }
---
+++ cp/day3.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+using namespace std;
+
+class Complex
+{
+private:
+    int real;
+    int imag;
+
+public:
+    // Constructor
+    Complex(int r = 0, int i = 0) : real(r), imag(i) {}
+
+    // Operator overloading for +
+    Complex operator+(const Complex &other)
+    {
+        return Complex(real + other.real, imag + other.imag);
+    }
+
+    // Function to display complex number
+    void display()
+    {
+        cout << real << " + " << imag << "i" << endl;
+    }
+};
+
+int main()
+{
+    Complex c1(3, 4);
+    Complex c2(1, 2);
+
+    Complex c3 = c1 + c2; // Uses overloaded + operator
+    // Complex c4 = c1 - c2; // CE error: no match for 'operator-' (operand types are 'Complex' and 'Complex')
+
+    c3.display(); // Output: 4 + 6i
+    return 0;
+}
+
+// #include <iostream>
+
+// using namespace std;
+
+// class Calc
+// {
+// public:
+//     int x;
+//     int operator add +  (int y)
+//     {
+//         return x + y;
+//     }
+// };
+
+// int main()
+// {
+
+//     Calc calc;
+//     calc.x = 10;                    // Initialize x
+//     int y = 20;                     // Another number to add
+//     int num = calc.add(y);          // Function call
+//     cout << "Sum: " << num << endl; // Output the result
+
+//     return 0;
+// }
+
+// // #include <iostream>
+
+// // using namespace std;
+
+// // class Calc
+// // {
+// // public:
+// //     int x;
+// //     int add(int y)
+// //     {
+// //         return x + y;
+// //     }
+// // };
+
+// // int main()
+// // {
+
+// //     Calc calc;
+// //     calc.x = 10;                    // Initialize x
+// //     int y = 20;                     // Another number to add
+// //     int num = calc.add(y);          // Function call
+// //     cout << "Sum: " << num << endl; // Output the result
+
+// //     return 0;
+// // }
+
+// // // int main()
+// // // {
+// // //     int x = 10, y = 20;
+// // //     int num = add(x, y);            // Function call
+// // //     cout << "Sum: " << num << endl; // Output the result
+
+// // //     return 0;
+// // // }
