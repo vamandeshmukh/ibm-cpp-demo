f9b0d92e6d5c0b3e62cd2fdbfdb42541098210db
Vaman Deshmukh <vamandeshmukh@gmail.com>
2025-08-19 11:52:28 +0530

    classes and its members

--- cp/day3class.cpp
+++ cp/day3class.cpp
@@ -1,5 +1,5 @@
 // class -
-// members - fields, methods, constructors, inner classes
+// members - fields, methods, constructors, inner classes, friends, outsiders
 // access - private
 // object - stack / heap
 // ======================
@@ -8,16 +8,28 @@
 #include <iostream>
 using namespace std;
 
+int num = 10;
+
 class Employee
 {
 
-public:
-    int id;
-    string name;
+private:
     double salary;
 
-    Employee() {}
+public:
+    // fields
+    int id;
+    string name;
 
+    // default constructor == no args constructor
+    Employee()
+    {
+        id = 0;
+        name = "";
+        salary = 0.0;
+    }
+
+    // parameterized constructor
     Employee(int i, string n, double s)
     {
         id = i;
@@ -25,25 +37,46 @@ public:
         salary = s;
     }
 
+    // member function
     void display()
     {
-        cout << "ID: " << id << ", Name: " << name << ", Salary: " << salary << endl;
+        cout << "ID: " << id << ", Name: " << name << ", Salary: " << salary << num << endl;
     }
+
+    // friend function declaration
+    friend void salaryCredited(Employee e);
+
+    // member function outside the class - declaration
+    void getBonus();
 };
+
+// firend function definition
+void salaryCredited(Employee e)
+{
+    cout << "Salary credited for " << e.name << ": " << e.salary << num << endl;
+}
+
+// member function outside the class - definition
+void Employee::getBonus()
+{
+    cout << "Bonus for " << name << ": " << salary * 0.1 << endl;
+}
 
 int main()
 {
     Employee emp1(101, "Sonu", 50000.0);
-    Employee *emp2 = new Employee(102, "Monu", 60000.0);
-    Employee *emp3 = new Employee();
-    Employee emp4;
     emp1.display();
-    emp2->display();
-    emp3->display();
-    emp4.display();
+    salaryCredited(emp1); // calling friend function
 
-    delete emp2;
-    delete emp3;
+    // Employee *emp2 = new Employee(102, "Monu", 60000.0);
+    // Employee *emp3 = new Employee();
+    // Employee emp4;
+    // emp1.display();
+    // emp2->display();
+    // emp3->display();
+    // emp4.display();
+    // delete emp2;
+    // delete emp3;
 
     return 0;
 }
