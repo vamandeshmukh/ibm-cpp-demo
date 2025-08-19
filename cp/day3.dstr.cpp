3835f7096f6f38d7cfdef13ed7c966b74306405c
Vaman Deshmukh <vamandeshmukh@gmail.com>
2025-08-19 14:36:36 +0530

    virtual

--- cp/day3.dstr.cpp
+++ cp/day3.dstr.cpp
@@ -1,19 +1,40 @@
 #include <iostream>
 using namespace std;
 
-class Employee
+class Person
 {
 public:
     void display()
     {
-        cout << "Employee is here!" << endl;
+        cout << "Person is here!" << endl;
     }
+    Person()
+    {
+        cout << "Person object created!" << endl;
+    }
+    ~Person()
+    {
+        cout << "Person object destroyed!" << endl;
+    }
+};
+
+class Employee : public Person
+{
+public:
+    // Employee()
+    // {
+    //     cout << "Employee object created!" << endl;
+    // }
+    // ~Employee()
+    // {
+    //     cout << "Employee object destroyed!" << endl;
+    // }
 };
 
 int main()
 {
     Employee *emp = new Employee();
     emp->display();
-    
+    delete emp;
     return 0;
 }
